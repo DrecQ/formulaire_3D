f13817627c93bd866eba1c7849f1609b6e1cd702
QUIST Evariste <drecquist@gmail.com>
2023-12-29 04:20:10 +0100

    Mon formulaire 3D

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Vous pouvez ajouter ici le code pour traiter les données du formulaire, comme envoyer une requête au serveur.
+  });
+  
